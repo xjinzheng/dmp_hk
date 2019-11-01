8b00538a53cddf3cda4f756f3ad9d5b3eee05b9d
Gin XiaoJin Zheng <Gin-XiaoJin-Zheng@users.noreply.github.com>
2019-11-01 18:01:55 +0800

    1101

--- tg_data/gagaga.R
+++ tg_data/gagaga.R
@@ -1,22 +1,21 @@
-setwd("~/Users/ginzheng/Desktop/Research/tech&protest")
+setwd("~/Documents/Github/decentralizedcommplatform/tg_data")
 
 library(lubridate)
 library(ggplot2)
 library(scales)
 
-channel_data <- read.csv('telegram_channel_data.csv')
-channel_data$Date <- as.Date(channel_data$Date)
-channel_data$Time <- as.POSIXct(channel_data$Time, format = "%H:%M")
-channel_data$Views <- as.numeric(channel_data$Views)
+fc_data<- read.csv('factcheck_data.csv')
+fc_data$Date <- as.Date(fc_data$Date)
+fc_data$Time <- as.POSIXct(fc_data$Time, format = "%H:%M")
+fc_data$Views <- as.numeric(fc_data$Views)
 
-daily_plot <- ggplot(channel_data, aes(x=Date, y=Views))
-daily_plot + geom_bar(stat="identity") + scale_x_date(breaks=date_breaks("2 weeks"))
-daily_plot + geom_line(colour="blue") + geom_point(colour="blue4") + scale_x_date(breaks=date_breaks("2 weeks"))
+fc_daily_plot <- ggplot(fc_data, aes(x=Date, y=Views))
+fc_daily_plot + geom_line(colour="blue") + geom_point(colour="blue4") + scale_x_date(breaks=date_breaks("2 weeks")) + scale_y_continuous(breaks=seq(0,60,5))
 
-ggplot(channel_data, aes(x=Time, y=Views, group=Date)) + 
-  geom_path(size=.5, alpha = 0.1, colour="blue") + 
-  scale_x_datetime(breaks=date_breaks("2 hour") +
-  scale_y_continuous(breaks = c(0, ))                  , labels=date_format("%H:%M"))
+fc_minute_plot <-  ggplot(fc_data, aes(x=Time, y=Views, group=Date))
+fc_minute_plot + geom_point(size=.5, alpha = 0.6, colour="blue") + 
+  scale_x_datetime(breaks=date_breaks("2 hour"), labels=date_format("%H:%M")) + 
+  scale_y_continuous(breaks=seq(0,60,5))
 
 
 
